94df93226aead859ccf284eeb27d06046228d04f
antimonycodes <akinrinadetobilobasb@gmail.com>
2025-02-24 10:36:07 +0100

    fix: correct path resolution for serving index.html in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(rootDir, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(rootDir, "frontend", "dist", "index.html"));
   });
 }
 
